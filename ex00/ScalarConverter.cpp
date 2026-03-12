6d7d1d13c4ceb6fe4e18bdcb49b074dd71b45a1c
ocviller <ocviller@student.42.fr>
2026-03-12 15:20:46 +0100

    rebuild tt depuis le debut

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: ocviller <ocviller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/05 13:55:08 by ocviller          #+#    #+#             */
-/*   Updated: 2026/03/06 12:48:28 by ocviller         ###   ########.fr       */
+/*   Updated: 2026/03/12 15:18:47 by ocviller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,63 +36,149 @@ ScalarConverter& ScalarConverter::operator=(const ScalarConverter &other)
 	return (*this);
 }
 
-int is_int(std::string str)
+int valid_char(int c)
 {
-    int flag = 0;
+    if (isdigit(c) || c == '+' || c == '-')
+        return (1);
+    return (0);
+}
+
+int find_type(std::string va, std::string *keywords)
+{
+    unsigned long i = 0;
+    int count_d = 0;
+    int count_f = 0;
     
-    for (int i = 0; str[i]; i++)
+    for (int i = 0; i < 6; i++)
     {
-        if (!isdigit(str[i]) && str[i] != '-' && str[i] != '+')
-            return (0);
-        if (isdigit(str[i]))
-            flag = 1;
+        if (keywords[i] == va || va.length() == 1 || va == "\\")
+            return (1);
     }
-    if (flag == 0)
-        return (0);
+    if (va[i] == '+' || va[i] == '-')
+        i++;
+    if (va.length() != 1 && !isdigit(va[i]))
+        return (std::cout << "3error: format\n", 0);
+    for (; va[i]; i++)
+    {
+        if (va[i] == '.')
+            count_d++;
+        if (va[i] == 'f')
+            count_f++;
+        if ((!isdigit(va[i]) && va[i] != '.' && va[i] != 'f') || (i > 0 && va[i - 1] == '.' && !isdigit(va[i])))
+            return (std::cout << "2error: format\n", 0);
+    }
+    if (count_d > 1 || count_f > 1 || va[va.length() - 1] == '.' || va[0] == '.' || (count_f == 1 && va[va.length() - 1] != 'f'))
+        return (std::cout << "1error: format\n", 0);
     return (1);
 }
 
-int is_float(std::string str)
+std::string find_typee(std::string va, std::string *keywords)
 {
-    int flag = 0;
-    
-    for (int i = 0; str[i]; i++)
+    unsigned long i = 0;
+    for (i = 0; i < 6; i++)
     {
-        if (!isdigit(str[i]) && str[i] != '.' && str[i] != 'f' && str[i] != '-' && str[i] != '+')
-            return (0);
-        if (isdigit(str[i]))
-            flag = 1;
+        if (keywords[i] == va)
+            return ("char");
     }
-    if (flag == 0)
-        return (0);
-    return (1);
+    if ((va.length() == 1 && !isdigit(va[0])) || va == "\\")
+        return ("char");
+    for (i = 0; va[i] && valid_char(va[i]); i++)
+    {}
+    if (i == va.length())
+        return ("int");
+    if (va[va.length() - 1] == 'f')
+        return ("float");
+    else
+        return ("double");
+    return ("error");
 }
 
-int is_double(std::string str)
+void char_print(double ref)
 {
-    int flag = 0;
-    
-    for (int i = 0; str[i]; i++)
-    {
-        if (!isdigit(str[i]) && str[i] != '.' && str[i] != '-' && str[i] != '+')
-            return (0);
-        if (isdigit(str[i]))
-            flag = 1;
-    }
-    if (flag == 0)
-        return (0);
-    return (1);
+    if (ref < 0 || ref > 127)
+        std::cout << "char: impossible\n";
+    else if (ref < 32 || ref == 127)
+        std::cout << "char: Non displayable\n";
+    else
+        std::cout << "char: '" << static_cast<char>(round(ref)) << "'\n";
 }
 
-std::string find_type(std::string str)
+void fromInt(double ref)
 {
-    std::string type = "char";
-    
-    if (is_float(str))
-        type = "float";
-    if (is_double(str))
-        type = "double";
-    if (is_int(str))
-        type = "int";
-    return (type);
+    std::cout << "THIS IS INT : " << ref << "\n";
+    char_print(ref);
+    std::cout << "int: " << ref << "\n";
+    std::cout << std::fixed << std::setprecision(1) << "float: " << static_cast<float>(ref) << "f\n";
+    std::cout << std::fixed << std::setprecision(1) << "double: " << static_cast<double>(ref) << "\n";
+}
+
+void fromChar(std::string va, std::string *keywords)
+{
+    std::cout << "THIS IS CHAR : " << va[0] << "\n";
+    for (int i = 0; i < 6; i++)
+    {
+        if (va == keywords[i])
+            std::cout << "char: impossible\n";
+    }
+    if (va == "\\")
+        std::cout << "\\\n";
+    std::cout << "char: '" << va[0] << "'\n";
+    std::cout << "int: " << static_cast<int>(va[0]) << "\n";
+    std::cout << std::fixed << std::setprecision(1) << "float: " << static_cast<float>(va[0]) << "f\n";
+    std::cout << std::fixed << std::setprecision(1) << "double: " << static_cast<double>(va[0]) << "\n";
+}
+
+void fromFloat(double ref)
+{
+    std::cout << "THIS IS FLOAT : " << ref << "\n";
+    char_print(ref);
+    std::cout << "int: " << static_cast<int>(round(ref)) << "\n";
+    if (round(ref) == ref)
+    {
+        std::cout << std::fixed << std::setprecision(1) << "float: " << ref << "f\n";
+        std::cout << std::fixed << std::setprecision(1) << "double: " << static_cast<double>(ref) << "\n";
+    }
+    else
+    {
+        std::cout << "float: " << ref << "f\n";
+        std::cout << "double: " << static_cast<double>(ref) << "\n";
+    }
+}
+
+void fromDouble(double ref)
+{
+    std::cout << "THIS IS DOUBLE : " << ref << "\n";
+    char_print(ref);
+    std::cout << "int: " << static_cast<int>(round(ref)) << "\n";
+    if (round(ref) == ref)
+    {
+        std::cout << std::fixed << std::setprecision(1) << "float: " << static_cast<float>(ref) << "f\n";
+        std::cout << std::fixed << std::setprecision(1) << "double: " << ref << "\n";
+    }
+    else
+    {
+        std::cout << "float: " << static_cast<float>(ref) << "f\n";
+        std::cout << "double: " << ref << "\n";
+    }
+}
+
+void ScalarConverter::convert(std::string va)
+{
+    //
+    std::string keywords[6] = {"+inf", "-inf", "+inff", "-inff", "nan", "nanf"};
+    std::cout << "STRING DE BASE : " << va << "\n";
+    if (!find_type(va, keywords))
+        return ;
+    std::string type = find_typee(va, keywords);
+    double ref = std::strtod(va.c_str(), NULL);
+    std::cout << "STRING TYPE : " << type << "\n";
+    if (type == "int")
+        fromInt(static_cast<int>(round(ref)));
+    if (type == "char")
+        fromChar(va, keywords);
+    if (type == "float")
+        fromFloat(static_cast<float>(ref));
+    if (type == "double")
+        fromDouble(ref);
+    std::cout << "\n\n";
 }
--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: ocviller <ocviller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/05 13:54:37 by ocviller          #+#    #+#             */
-/*   Updated: 2026/03/06 16:31:13 by ocviller         ###   ########.fr       */
+/*   Updated: 2026/03/12 14:15:17 by ocviller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,6 @@
 #include <iomanip>
 #include <limits>
 
-int parse_char(std::string va);
-int max_parsing(std::string va, std::string type);
-std::string find_type(std::string str);
-void convert_char(std::string va, std::string type, long long nbr, float nbr_f, double nbr_d);
-void convert_int(std::string va, std::string type, long long nbr, float nbr_f, double nbr_d);
-void convert_float(std::string va, std::string type, long long nbr, float nbr_f, double nbr_d);
-void convert_double(std::string va, std::string type, long long nbr, float nbr_f, double nbr_d);
-void print_nan(std::string va, char t);
-
 class ScalarConverter
 {
     private:
@@ -37,23 +28,6 @@ class ScalarConverter
     ScalarConverter(const ScalarConverter &other);
     ScalarConverter& operator=(const ScalarConverter &other);
     ~ScalarConverter();
-
-    static void convert(std::string va)
-    {
-        std::string type = find_type(va);
-        std::cout << "STRING DE BASE : " << va << "\n";
-        std::cout << "TYPE DE LA STRING : " << type << "\n\n";
-        if (type == "char" && !parse_char(va))
-            return ;
-        else if (type != "char" && !max_parsing(va, type))
-            return ;
-        long long nbr = atoll(va.c_str());
-        float nbr_f = static_cast<float>(atof(va.c_str()));
-        double nbr_d = atof(va.c_str());
-        convert_char(va, type, nbr, nbr_f, nbr_d);
-        convert_int(va, type, nbr, nbr_f, nbr_d);
-        convert_float(va, type, nbr, nbr_f, nbr_d);
-        convert_double(va, type, nbr, nbr_f, nbr_d);
-        std::cout << "\n\n";
-    }
+    
+    static void convert(std::string va);
 };
